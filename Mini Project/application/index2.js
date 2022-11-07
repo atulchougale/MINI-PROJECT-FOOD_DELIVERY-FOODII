a3ed37183d655b356dba4b4c4097e09fd3e08b00
Atul Chougale <106691618+atulchougale@users.noreply.github.com>
2022-11-07 23:53:52 +0530

    Add files via upload

---
+++ Mini Project/application/index2.js
@@ -0,0 +1,539 @@
+const foodItem= [
+    {
+    id: 1,
+    id1:1,
+    name: 'Ambur Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'https://i.ibb.co/gwzcwf1/Ambur-Chicken-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    id1:2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'https://i.ibb.co/ZxdjQc6/Chicken-Biryani-hyd.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    id1:3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'https://i.ibb.co/sJgLQdv/egg-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    id1:4,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'https://i.ibb.co/hm0RTZ2/goan-fish-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    id1:5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 10,
+    img: 'https://i.ibb.co/YfQMLV7/hyd-Mutton-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    id1:6,
+    name: 'Kamrupi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 12,
+    img: 'https://i.ibb.co/Wp8WFL7/kamrupi-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    id1:7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'https://i.ibb.co/Z68fzhk/kashmiri-pulao.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    id1:8,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 20,
+    img: 'https://i.ibb.co/6FcmkB2/memonibiryani.png',
+    quantity: 1
+},
+{
+    id: 9,
+    id1:9,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'https://i.ibb.co/HtSfLxC/mughlai-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    id1:10,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: 'https://i.ibb.co/0GbVd4m/Chicken-roast.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    id1:11,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: 'https://i.ibb.co/qFrrhrx/Chicken-Curry.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    id1:12,
+    name: 'Chicken Do Pyaza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 14,
+    img: 'https://i.ibb.co/yyYSKmC/Chicken-do-Pyaza.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    id1:13,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 4.3,
+    price: 12,
+    img: 'https://i.ibb.co/7Ys1v0w/Chicken-Masala.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    id1:14,
+    name: 'Handi Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: 'https://i.ibb.co/nCp4zG9/Handi-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    id1:15,
+    name: 'Murgh Musallam',
+    category : 'chicken',
+    rating : 4.3,
+    price: 20,
+    img: 'https://i.ibb.co/NtJq0ym/Murgh-Musallam.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    id1:16,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'https://i.ibb.co/F88wVL4/Matar-Paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    id1:17,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 10,
+    img: 'https://i.ibb.co/VwJpSwR/palak-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    id1:18,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'https://i.ibb.co/88vPKyk/paneer-butter-masala.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    id1:19,
+    name: 'Paneer Do Pyaza',
+    category : 'paneer',
+    rating : 4.3,
+    price: 12,
+    img: 'https://i.ibb.co/qrnGvXt/Paneer-Do-Pyaza.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    id1:20,
+    name: 'Hyderabadi Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 8,
+    img: 'https://i.ibb.co/3WjD7s2/Paneer-Hyderabadi.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    id1:21,
+    name: 'Paneer Lababdar',
+    category : 'paneer',
+    rating : 4.3,
+    price: 7,
+    img: 'https://i.ibb.co/Z11snpj/paneer-lababdar.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    id1:22,
+    name: 'Shahi Paneer',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 5,
+    img: 'https://i.ibb.co/MhvC4x1/shahi-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    id1:23,
+    name: 'Navratan Korma',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 8,
+    img: 'https://i.ibb.co/z5TVz0T/navratan-korma-vegetable.png',
+    quantity: 1
+},
+{
+    id: 24,
+    id1:24,
+    name: 'Veg Jalfrezi',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'https://i.ibb.co/QCLnhcr/VEG-JALFREZI.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    id1:25,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 5,
+    img: 'https://i.ibb.co/s2J2B1D/vegetable-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    id1:26,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'https://i.ibb.co/L9q3gtk/vegetable-curry.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    id1:27,
+    name: 'Veg Kolhapur',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 10,
+    img: 'https://i.ibb.co/NmnCbT2/vegetable-kolhapuri.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    id1:28,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'https://i.ibb.co/yX8fS8r/vegetable-masala.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    id1:29,
+    name: 'Veg Pakora',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'https://i.ibb.co/khhHdCs/vegetable-pakora.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    id1:30,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'https://i.ibb.co/NLkhKqp/cabbage-momos.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    id1:31,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: 'https://i.ibb.co/DCpbFsg/Chicken-Manchurian.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    id1:32,
+    name: 'Chili Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: 'https://i.ibb.co/DK862MD/Chili-Chicken.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    id1:33,
+    name: 'Chowmein',
+    category : 'chinese',
+    rating : 4.3,
+    price: 16,
+    img: 'https://i.ibb.co/9hbrRYK/chowmin.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    id1:34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 14,
+    img: 'https://i.ibb.co/3WCzt4z/spring-rolls.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    id1:35,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'https://i.ibb.co/rHB3C1z/szechuan-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    id1:36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'https://i.ibb.co/r2wycF3/veg-fried-rice.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    id1:37,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 18,
+    img: 'https://i.ibb.co/Ns5xgbv/Butter-Masala-Dosa.png',
+    quantity: 1
+},
+{
+    id: 38,
+    id1:38,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 4.3,
+    price: 20,
+    img: 'https://i.ibb.co/C02rPKr/idli-with-rice-flour.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    id1:39,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 12,
+    img: 'https://i.ibb.co/1TNLMcW/masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    id1:40,
+    name: 'Mysore Bonda',
+    category : 'south indian',
+    rating : 4.3,
+    price: 10,
+    img: 'https://i.ibb.co/CndymFV/mysore-bonda.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    id1:41,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 15,
+    img: 'https://i.ibb.co/yF7s3Y8/onion-uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    id1:42,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 40,
+    img: 'https://i.ibb.co/M6fcM23/plain-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    id1:43,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 25,
+    img: 'https://i.ibb.co/vBfgtDX/Rava-Uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    id1:44,
+    name: 'Sambhar Vada',
+    category : 'south indian',
+    rating : 4.3,
+    price: 34,
+    img: 'https://i.ibb.co/Yhdk5pn/sambhar-vada.jpg',
+    quantity: 1
+},
+]
+
+document.querySelectorAll('.add-to-cart').forEach(item=>{
+    item.addEventListener('click',checkToCart)
+    
+})
+
+
+var cartData2= [];
+function checkToCart(){
+    
+    var itemToAdd2= this.parentNode.nextSibling.nextSibling.innerText;
+    var itemObj= foodItem.find(element=>element.name==itemToAdd2);
+
+    var index= cartData2.indexOf(itemObj);
+    if(index=== -1){
+        document.getElementById(itemObj.id).classList.add('toggle-cart');
+        cartData= [...cartData2,itemObj];
+    }
+    else if(index > -1){
+        alert("Added to cart!");
+    }
+    
+    document.getElementById('check_btn').innerText=
+    ' ' + cartData2.length + ' Ckeckout';
+ 
+   
+    
+    
+    checkItems();
+}
+
+
+function checkItems(){
+    var tableBody2=  document.getElementById('table-body2');
+    tableBody2.innerHTML= '';
+
+    cartData2.map(item=> {
+        var tableRow1= document.createElement('tr');
+        
+        var rowData12= document.createElement('td');
+        var img= document.createElement('img');
+        img.src= item.img;
+        rowData12.appendChild(img);
+    
+        var rowData23= document.createElement('td');
+        rowData23.innerText= item.name;
+        
+        var rowData34= document.createElement('td');
+        
+        var span= document.createElement('span');
+        span.innerText= item.quantity;
+       
+        
+        
+        rowData34.appendChild(span);
+       
+    
+        var rowData45= document.createElement('td');
+        rowData45.innerText= item.price;
+    
+        tableRow1.appendChild(rowData12);
+        tableRow1.appendChild(rowData23);
+        tableRow1.appendChild(rowData34);
+        tableRow1.appendChild(rowData45);
+    
+        tableBody2.appendChild(tableRow1);
+    })
+  
+}
+
+
+
+
+document.getElementById('check_btn').addEventListener('click',checkToggle);
+
+
+var flag3= false;
+function checkToggle(){
+    if(cartData2.length > 0){
+        document.getElementById('food-items').classList.toggle('food-items');
+        document.getElementById('category-list').classList.toggle('food-items');
+        document.getElementById('category-header').classList.toggle('toggle-category');
+        document.getElementById('m-cart-plus').classList.toggle('m-cart-toggle')
+        document.getElementById('checkout-page').classList.toggle('checkout-toggle');
+       
+       
+        flag3= true;
+        console.log(flag3)
+    }
+    else{
+        alert("Currently no item in Checkout!");
+    }
+}
